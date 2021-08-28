e261cfa60ef43cc498ca2defb53e61efd5ab82a1
kid <vip92x@163.com>
2021-08-28 10:56:16 +0800

    使用并发方式post数据

--- umsapi.cpp
+++ umsapi.cpp
@@ -5,6 +5,7 @@
 #include "Common/utility.h"
 #include "CrashReporter/minidump-analyzer.h"
 #include "UMS/umsmanager.h"
+#include "Common/ThreadPool.h"
 
 #if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || (defined(__cplusplus) && __cplusplus >= 201703L)) && defined(__has_include)
 #if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
@@ -27,6 +28,8 @@ static bool isNewSession;
 static std::string device_resolution;
 static bool isValidKey = false;
 
+static ThreadPool pool(std::thread::hardware_concurrency());
+
 static void initLogger()
 {
     Configurations c;
@@ -42,6 +45,7 @@ static void initLogger()
 
 static void registerEvent()
 {
+    // noop
 }
 
 static bool isAppkeyValid(const std::string& appkey)
@@ -84,8 +88,10 @@ void UMSApi::bindApplicationVersion(const std::string& version)
 void UMSApi::postClientdata()
 {
     if (isValidKey) {
-        manager.clientDataProceed();
-        manager.allDataProceed();
+        pool.enqueue([]{
+            manager.clientDataProceed();
+            manager.allDataProceed();
+        });
     } else {
         LOG(ERROR) << "not valid appkey!";
     }
@@ -93,22 +99,31 @@ void UMSApi::postClientdata()
 
 void UMSApi::onEvent(const std::string& event_id, const std::string& pagename)
 {
-    manager.eventDataProceed(event_id, pagename);
+    pool.enqueue([](const std::string& event_id, const std::string& pagename){
+        manager.eventDataProceed(event_id, pagename);
+    }, event_id, pagename);
 }
 
 void UMSApi::onEvent(const std::string& event_id, const std::string& pagename, int acc)
 {
-    manager.eventDataProceed(event_id, pagename, "", acc);
+    pool.enqueue([](const std::string& event_id, const std::string& pagename, int acc) {
+        manager.eventDataProceed(event_id, pagename, "", acc);
+    }, event_id, pagename, acc);
+
 }
 
 void UMSApi::onEvent(const std::string event_id, const std::string& pagename, const std::string& label)
 {
-    manager.eventDataProceed(event_id, pagename, label);
+    pool.enqueue([](const std::string event_id, const std::string& pagename, const std::string& label) {
+        manager.eventDataProceed(event_id, pagename, label);
+    }, event_id, pagename, label);
 }
 
 void UMSApi::onEvent(const std::string& event_id, const std::string& pagename, const std::string& label, int acc)
 {
-    manager.eventDataProceed(event_id, pagename, label, acc);
+    pool.enqueue([](const std::string& event_id, const std::string& pagename, const std::string& label, int acc) {
+        manager.eventDataProceed(event_id, pagename, label, acc);
+    }, event_id, pagename, label, acc);
 }
 
 void UMSApi::onPageBegin(const std::string& name)
@@ -121,52 +136,66 @@ void UMSApi::onPageBegin(const std::string& name)
 void UMSApi::onPageEnd(const std::string& name)
 {
     if (!name.empty()) {
-        UmsManager::getInstance().addPageEnd(name);
+        pool.enqueue([](const std::string& name) {
+            UmsManager::getInstance().addPageEnd(name);
+        }, name);
     }
 }
 
 void UMSApi::postTag(const std::string& tag)
 {
-    manager.tagDataProceed(tag);
+    pool.enqueue([](const std::string& tag) {
+        manager.tagDataProceed(tag);
+
+    }, tag);
 }
 
 void UMSApi::postUserid(const std::string& userid)
 {
-    manager.useridDataProceed(userid);
+    pool.enqueue([](const std::string& userid) {
+        manager.useridDataProceed(userid);
+    }, userid);
 }
 
 void UMSApi::postPushid(const std::string& pushid)
 {
-    manager.pushidDataProceed(pushid);
+    pool.enqueue([](const std::string& pushid) {
+        manager.pushidDataProceed(pushid);
+    }, pushid);
 }
 
 void UMSApi::postCrashData(const std::string& dumpdir)
 {
-    std::vector<std::string> dumps;
-    if (fs::is_directory(dumpdir)) {
-        for (auto& p : fs::recursive_directory_iterator(dumpdir)) {
-            if (p.path().extension() == ".dmp") {
-                dumps.push_back(p.path().string());
+    pool.enqueue([](const std::string& dumpdir) {
+        std::vector<std::string> dumps;
+        if (fs::is_directory(dumpdir)) {
+            for (auto& p : fs::recursive_directory_iterator(dumpdir)) {
+                if (p.path().extension() == ".dmp") {
+                    dumps.push_back(p.path().string());
+                }
+            }
+        } else {
+            dumps.push_back(dumpdir);
+        }
+        // Parse minidump and post
+        for (auto& dump : dumps) {
+            Json::Value stacktraceJS;
+            bool ret = CrashReporter::ProcessMinidump(stacktraceJS, dump);
+            if (ret) {
+                std::uint32_t stamp = stacktraceJS["crash_info"]["time"].asUInt();
+                std::string time = Utility::timeDataStampToString(stamp);
+                manager.crashDataProceed(time, stacktraceJS.toStyledString());
+                std::string dump_bak = dump + ".bak";
+                fs::rename(dump, dump_bak);
             }
         }
-    } else {
-        dumps.push_back(dumpdir);
-    }
 
-    for (auto& dump : dumps) {
-        Json::Value stacktraceJS;
-        bool ret = CrashReporter::ProcessMinidump(stacktraceJS, dump);
-        if (ret) {
-            std::uint32_t stamp = stacktraceJS["crash_info"]["time"].asUInt();
-            std::string time = Utility::timeDataStampToString(stamp);
-            manager.crashDataProceed(time, stacktraceJS.toStyledString());
-            std::string dump_bak = dump + ".bak";
-            fs::rename(dump, dump_bak);
-        }
-    }
+    }, dumpdir);
 }
 
 void UMSApi::updateOnlineConfig()
 {
-    manager.onlineConfigProceed();
+    pool.enqueue([]{
+        manager.onlineConfigProceed();
+    });
 }
